0344a09b3c14dd2efd625c790282cabb69b4cd8c
Alessandro Trigilio <alessandro@thesocialus.com>
2017-05-30 18:47:58 -0400

    ass

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true  #Status camniado
-  # config.serve_static_assets = false #ATP: Linea agregada
+  config.serve_static_assets = false #ATP: Linea agregada
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
