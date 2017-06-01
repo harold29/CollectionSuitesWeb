4ae4b6164946797c800725767b87a0eca67aa0bc
DsEsteban <dennis@thesocialus.com>
2017-06-01 19:17:35 -0400

    fonts -v4

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,8 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
+  
+  config.assets.paths << "#{Rails.root}/app/assets/fonts"
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
